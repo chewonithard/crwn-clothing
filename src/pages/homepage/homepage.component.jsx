e3b4dbba0ad75c944068c9dc9d88659803bcc40f
Nicholas Chew <chewonithard@gmail.com>
2022-02-07 22:19:07 +0800

    added routes

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import Directory from '../../components/directory/directory-component'
 import './homepage.styles.scss';
 
-const HomePage = () => (
-  <div className='homepage'>
+const HomePage = (props) => (
+ <div className='homepage'>
     <Directory />
-
   </div>
 );
 
