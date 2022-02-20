4fb6c6e59fcb98835c3981dcee30161425f23b6b
Nicholas Chew <chewonithard@gmail.com>
2022-02-20 13:23:12 +0800

    implemented firebase utils, included ability to store authenticated users into firestore DB

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils' // need to import currentuser state here. hence used class componenet
 
 class App extends React.Component {
   constructor() {
@@ -16,16 +16,40 @@ class App extends React.Component {
     }
   }
 
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null // onAuthStateChanged is an OPEN subscription between app and firebase. need to close it when user signs out, otherwise there will be memory leaks from our app. null is assigned as default value here.
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => { // componentdidmount will only fetch data once. we want to always know if user is signed in our out. hence auth.onAuthStateChanged is a method from firebase that helps to do that. firebase gives persistence of user until signed out
+      // this.setState({ currentUser: user });
+      // console.log(user) // user object gives us all the impt stuff like name, email
+
+    // createUserProfileDocument(user)
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth); // check if DB has updated with new data
+
+        userRef.onSnapshot ( snapshot => {  // onSnapshot similar to calling onAuthStateChanged
+          // cl snapshot
+          // cl snapshot.data() has no id
+          this.setState(
+            {
+              currentUser: {
+                id: snapshot.id,
+                ...snapshot.data(),
+              },
+            },
+            // () => console.log(this.state) // cannot call this after setState as it is asynchronous i.e. setState not finished when u call CL, so need to pass 2nd function inside setState
+          );
+        })
+        console.log(this.state)
+      } else {
+        this.setState({currentUser: userAuth}) // if user logs out, set it to null
+      }
+
     });
   }
 
   componentWillUnmount() {
-    this.unsubscribeFromAuth();
+    this.unsubscribeFromAuth(); // closes the subscription
   }
 
   render() {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,13 +12,45 @@ const config = {
   measurementId: "G-ZKNH75RG4G",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {  // passed into app.js
+  if (!userAuth) return; // if user signed out userAuth object is null
+
+  // query inside firestore if obj exists. firestore library gives either 1)queryreference or 2)querysnapshot. and these will be in either Document or Collection firestore always returns either of this, even if nothing exists from that query.
+
+  // console.log(firestore.doc('users/adodjfaiojfa'))
+  // documentreference vs collection reference. docref obj allows CRUD methods. Get snapshot object using .get(). documentRef.get() or collectionRef.get(). former returns a documentSnapshot object, latter retruns querySnapshot obj.
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`); // check if user exists
+  const snapShot = await userRef.get(); // get snapshot obj
+  // console.log(snapShot) // has an 'exists' property
+
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    // if snapshot doesn't exist, create this new user data below
+    try {
+      await userRef.set({ // asynchronous request to db to store this data - check firestore to see new creation
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
+  }
+
+  return userRef; // chance that we want to use userRef obj to do something else
+}
+
+
 firebase.initializeApp(config);
 
-export const auth = firebase.auth()
+export const auth = firebase.auth() // exported to app.js
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' })
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const signInWithGoogle = () => auth.signInWithPopup(provider); // signinwithpopup can use many methods of signing in, twitter, fb etc. we only want the google one. This func is exported to sign-in.component, to create a custom button onclick signinwithgoogle
 
-export default firebase;
+export default firebase; // in case we want the whole library
